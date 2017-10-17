0666aa4d2e6231724b9d517d7435eecb9535782a
Lukas Bier <lukasbier95@gmail.com>
2017-10-17 16:49:01 +0200

    removed uk-card elements

--- tracker.php
+++ tracker.php
@@ -104,10 +104,9 @@ if (!empty($task)) {
 // Feld mit dem result text erstellen (oder auch nicht)
 if (!empty($result_text)) {
 	$content = '<div class="uk-width-1-1">
-		<div class="uk-card uk-card-default">
-			<div id="result_text" class="uk-card-header">
-				<p>' . $result_text . '</p>
-			</div>
+		<div class="uk-alert" data-uk-alert>
+			<a href="" class="uk-alert-close uk-close"></a>
+            <p>' . $result_text . '</p>
 		</div>
 	</div>';
 } else {
@@ -142,75 +141,73 @@ if (isset($task) && $task == "add") {
     $mods_checkboxes .= '</div>';
 
 	$content .= '<div class="uk-width-1-1">
-		<div class="uk-card uk-card-default">
-			<div class="uk-card-header">
-				<h3 class="uk-card-title">Add a run</h3>
-			</div>
-			<div class="uk-card-body">
-				<form class="uk-form uk-form-stacked" action="index.php" method="post">
-                    <div class="uk-grid uk-grid-medium" data-uk-grid-margin>
-                        <div class="uk-width-1-2">
-                            <label>DLC</label>
-                            ' . $dlc_dropdown . '
-                        </div>
-                        <div class="uk-width-1-2">
-                            <label>Map</label>
-                            <select id="map_dropdown" class="uk-select uk-width-1-1" name="run[map_id]">
-                            </select>
-                        </div>
-                        <div class="uk-width-1-3">
-                            <label>Difficulty</label>
-                            ' . $difficulty_dropdown . '
-                        </div>
-                        <div class="uk-width-1-3">
-                            <label>Duration (min)</label>
-                            <input class="uk-input uk-width-1-1" type="number" name="run[duration]">
-                        </div>
-                        <div class="uk-width-1-3">
-                            <input id="xRed" type="checkbox" name="run[xRed]">
-                            <label for="xRed">Got Red Item</label>
-                        </div>
-                        <div class="uk-width-1-3">
-                            <label>Grimoires</label>
-                            <select class="uk-select uk-width-1-1" name="pro[grimoire_dice]">
-                                <option>0</option>
-                                <option>1</option>
-                                <option>2</option>
-                            </select>
-                        </div>
-                        <div class="uk-width-1-3">
-                            <label>Tomes</label>
-                            <select class="uk-select uk-width-1-1" name="pro[tome_dice]">
-                                <option>0</option>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                            </select>
-                        </div>
-                        <div class="uk-width-1-3">
-                            <label>Extra Dice</label>
-                            <select class="uk-select uk-width-1-1" name="pro[extra_dice]">
-                                <option>0</option>
-                                <option>1</option>
-                                <option>2</option>
-                            </select>
-                        </div>
-                        <div class="uk-width-1-1">
-                            <label>Mods</label>
-                            ' . $mods_checkboxes . '
-                        </div>
-                        <div class="uk-width-1-2">
-                            <label>Notes</label>
-                            <textarea class="uk-width-1-1" name="run[notes]"></textarea>
-                        </div>
-                        <div class="uk-width-1-1">
-                            <input type="hidden" name="task" value="add">
-                            <input class="uk-button" type="submit" name="submit" value="Add">
-                        </div>
+        <div class="header">
+            <h2>Add a run</h2>
+        </div>
+        <div class="body">
+            <form class="uk-form uk-form-stacked" action="index.php" method="post">
+                <div class="uk-grid uk-grid-medium" data-uk-grid-margin>
+                    <div class="uk-width-1-2">
+                        <label>DLC</label>
+                        ' . $dlc_dropdown . '
                     </div>
-				</form>
-			</div>
-		</div>
+                    <div class="uk-width-1-2">
+                        <label>Map</label>
+                        <select id="map_dropdown" class="uk-select uk-width-1-1" name="run[map_id]">
+                        </select>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <label>Difficulty</label>
+                        ' . $difficulty_dropdown . '
+                    </div>
+                    <div class="uk-width-1-3">
+                        <label>Duration (min)</label>
+                        <input class="uk-input uk-width-1-1" type="number" name="run[duration]">
+                    </div>
+                    <div class="uk-width-1-3">
+                        <input id="xRed" type="checkbox" name="run[xRed]">
+                        <label for="xRed">Got Red Item</label>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <label>Grimoires</label>
+                        <select class="uk-select uk-width-1-1" name="pro[grimoire_dice]">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <label>Tomes</label>
+                        <select class="uk-select uk-width-1-1" name="pro[tome_dice]">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                    </div>
+                    <div class="uk-width-1-3">
+                        <label>Extra Dice</label>
+                        <select class="uk-select uk-width-1-1" name="pro[extra_dice]">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </div>
+                    <div class="uk-width-1-1">
+                        <label>Mods</label>
+                        ' . $mods_checkboxes . '
+                    </div>
+                    <div class="uk-width-1-2">
+                        <label>Notes</label>
+                        <textarea class="uk-width-1-1" name="run[notes]"></textarea>
+                    </div>
+                    <div class="uk-width-1-1">
+                        <input type="hidden" name="task" value="add">
+                        <input class="uk-button" type="submit" name="submit" value="Add">
+                    </div>
+                </div>
+            </form>
+        </div>
 	</div>';
 	
 } else {
@@ -240,71 +237,69 @@ if (isset($task) && $task == "add") {
 //    exit;
 
 	$content .= '<div class="uk-width-1-1">
-		<div class="uk-card uk-card-default">
-			<div class="uk-card-header">
-				<h3 class="uk-card-title uk-float-left">Run Overview</h3>
-			</div>
-			<div class="uk-card-body">
-				<form class="uk-float-right" action="index.php" method="post">
-					<input type="hidden" name="task" value="add">
-					<input class="uk-button" type="submit" value="Add run">
-				</form>
-				<table class="uk-table uk-table-striped">
-					<colgroup>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col>
-						<col width="10%">
-					</colgroup>
-					<thead>
-						<tr>
-							<th>Difficulty</th>
-							<th>Mods</th>
-							<th>Map</th>
-							<th>Duration</th>
-							<th>Dice</th>
-							<th>red %</th>
-							<th>got a red</th>
-							<th>Notes</th>
-							<th>Date</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>';
-						if (!empty($runs)) {
-							foreach ($runs as &$run) {
-								$content .= '<tr>
-									<td>' . $run['dif_name'] . '</td>
-									<td>' . $run['rendered_mods'] . '</td>
-									<td>' . $run['map_name'] . '</td>
-									<td>' . $run['run_duration'] . ' min</td>
-									<td>' . $run['pro_dice_string'] . '</td>
-									<td>' . $run['run_probability_red'] . '%</td>
-									<td>' . ($run['run_xRed'] == 1 ? 'Yes' : 'No') . '</td>
-									<td>' . $run['run_notes'] . '</td>
-									<td>' . date("d.m.Y H:i", strtotime($run['run_createDtTi'])) . '</td>
-									<td>
-										<form action="index.php" method="post">
-                                            <input type="hidden" name="task" value="delete">
-                                            <input type="hidden" name="id_run" value="' . $run['id_run'] . '">
-                                            <button type="submit" title="Delete" onClick="return confirm(\'Delete run?\')"><i class="uk-icon-trash"></i></button>
-                                        </form>
-									</td>
-								</tr>';
-							}
-						} else {
-							$content .= '<tr><td colspan="8">No runs available</td></tr>';
-						}
-					$content .= '</tbody>
-				</table>
-			</div>
-		</div>
+        <div class="header">
+            <h2 class="uk-float-left">Run Overview</h2>
+        </div>
+        <div class="body">
+            <form class="uk-float-right" action="index.php" method="post">
+                <input type="hidden" name="task" value="add">
+                <input class="uk-button" type="submit" value="Add run">
+            </form>
+            <table class="uk-table uk-table-striped">
+                <colgroup>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col width="5%">
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>Difficulty</th>
+                        <th>Mods</th>
+                        <th>Map</th>
+                        <th>Duration</th>
+                        <th>Dice</th>
+                        <th>red %</th>
+                        <th>got a red</th>
+                        <th>Notes</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>';
+                    if (!empty($runs)) {
+                        foreach ($runs as &$run) {
+                            $content .= '<tr>
+                                <td>' . $run['dif_name'] . '</td>
+                                <td>' . $run['rendered_mods'] . '</td>
+                                <td>' . $run['map_name'] . '</td>
+                                <td>' . $run['run_duration'] . ' min</td>
+                                <td>' . $run['pro_dice_string'] . '</td>
+                                <td>' . $run['run_probability_red'] . '%</td>
+                                <td>' . ($run['run_xRed'] == 1 ? 'Yes' : 'No') . '</td>
+                                <td>' . $run['run_notes'] . '</td>
+                                <td>' . date("d.m.Y H:i", strtotime($run['run_createDtTi'])) . '</td>
+                                <td>
+                                    <form action="index.php" method="post">
+                                        <input type="hidden" name="task" value="delete">
+                                        <input type="hidden" name="id_run" value="' . $run['id_run'] . '">
+                                        <button class="uk-button-secondary" type="submit" title="Delete" onClick="return confirm(\'Delete run?\')"><i class="uk-icon-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>';
+                        }
+                    } else {
+                        $content .= '<tr><td colspan="8">No runs available</td></tr>';
+                    }
+                $content .= '</tbody>
+            </table>
+        </div>
 	</div>';
 }
 
